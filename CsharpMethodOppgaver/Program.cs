861620dbd7ebfa9d68aae9bc00251a8e99ae6131
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-11-15 14:39:14 +0100

    Add project files.

---
+++ CsharpMethodOppgaver/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+    
+//Oppgave 1
+decimal num1 = 5.5m;
+decimal num2 = 5.5m;
+
+decimal SummarizeNums(decimal num1, decimal num2)
+{
+    return num1 + num2;
+}
+Console.WriteLine($"{SummarizeNums(num1,num2)}");
+
+//Oppgave 2
+void MakeText()
+{
+    Console.WriteLine("This Method doesn't return anything c:");
+}
+
+SummarizeNums(num1, num2);
+MakeText();
